fdd69dcbc519a4ec9f43bf9f0887870bb8e4a83f
Liam-So <60020795+Liam-So@users.noreply.github.com>
2022-01-06 15:56:55 -0400

    update description

--- src/components/Timeline/Data.js
+++ src/components/Timeline/Data.js
@@ -1,32 +1,32 @@
 export const job1 = {
-    title: "Software Developer",
-    description1: "Developed implementation of various features in Java Kafka Streams applications that supported tier 1 automative and entertainment clients.",
-    description2: "Enhanced a RESTful API application to accommodate heavy traffic users for specific data entities.",
-    description3: "Participated in software redesigns to make code more flexible, readable, and robust. Relieved applications of technical debt to improve the longevity of the codebase.",
+    title: "Full Stack Developer",
+    description1: "Designed and debugged frontend components for new features using VueJs, Jest, and Cypress.",
+    description2: "Developed Flask endpoint to implement priority-based queue for Celery tasks, helping improve service performance by 50 percent.",
+    description3: "Integrated new social media platform to allow end-users to view over 20 new metrics on their dashboard.",
     direction: "left",
-    company: "Gracenote Inc.",
+    company: "Dash Hudson",
     year: "2021",
-    month: "Jan-Apr"
+    month: "Sep-Dec"
 }
 
 export const job2 = {
     title: "Software Developer",
-    description1: "Developed a PHP application to generate dynamic PDF reports to summarize mammographic image quality using detailed list of cases, charts, and tables to present information over a review period.",
-    description2: "Developed automated testing scripts using Selenium and Python to verify functionality of the web-application, reducing testing time by over 33 percent.",
-    description3: "Developed Verification Test Plan to test the web-application using interactive methods.",
+    description1: "Developed implementation of various features in Java Kafka Streams applications that supported tier 1 automotive and entertainment clients.",
+    description2: "Enhanced a RESTful API application to accommodate heavy traffic by users for specific data entities.",
+    description3: "Relieved applications of technical debt to improve the longevity of the codebase.",
     direction: "right",
     company: "Densitas Inc.",
     year: "2020",
-    month: "May-Aug"
+    month: "Jan-Apr"
 }
 
 export const job3 = {
-    title: "Camp Counsellor",
-    description1: "Supervised and provided leadership for over 30 campers to ensure a safe and caring environment.",
-    description2: "Organized and prepared daily camp activities for children ages five through twelve. ",
-    description3: "Interacted with parents and facility members in an energetic and professional manner.",
+    title: "Software Developer",
+    description1: "Developed a PHP application that generates dynamic PDF reports to summarize mammographic image quality.",
+    description2: "Designed Selenium Python scripts to automate end-to-end verification of the web-application, reducing testing time by 33 percent.",
+    description3: "Created Verification Test Plan to document cases to test the web-application using interactive methods.",
     direction: "left",
-    company: "Canada Games Centre",
-    year: "2015-19",
-    month: "Jul-Sep"
+    company: "Densitas Inc.",
+    year: "2020",
+    month: "May-Aug"
 }
