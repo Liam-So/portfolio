d7d899fb24a88cb3485b0fd96db92ebb3bfaa0ca
Liam So <Liam.So@dal.ca>
2022-01-06 22:09:07 -0400

    date change

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ function Main() {
         <section className="bg-white mt-20 mb-20">
             <div className="max-w-2xl px-6 text-center mx-auto">
                 <h2 className="text-3xl font-semibold text-gray-800 tracking-wide">Hi, I’m Liam. Nice to meet you.</h2>
-                <p className="text-gray-600 mt-4">I'm a 4th year Computer Science student at Dalhousie University seeking employment opportunities for the Fall of 2021.</p>
+                <p className="text-gray-600 mt-4">I'm a 4th year Computer Science student at Dalhousie University seeking employment opportunities for the Summer of 2022.</p>
 
                 <div className="flex items-end justify-center mt-16">
                     <img className="h-56 w-56" src={Profile} alt="profile_pic" />
